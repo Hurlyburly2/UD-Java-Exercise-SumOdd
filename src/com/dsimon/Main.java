fd57e5fede48c21f886ecafe1b985823d133caea
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-21 00:53:14 -0400

    exercise complete

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -3,6 +3,11 @@ package com.dsimon;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(SumOdd.sumOdd(1, 100) == 2500);
+        System.out.println(SumOdd.sumOdd(-1, 100) == -1);
+        System.out.println(SumOdd.sumOdd(100, 100) == 0);
+        System.out.println(SumOdd.sumOdd(13, 13) == 13);
+        System.out.println(SumOdd.sumOdd(100, -100) == -1);
+        System.out.println(SumOdd.sumOdd(100, 1000) == 247500);
     }
 }
